bd4f9daef5bb3edfb20a0af22e3b93b25d975d1b
SmitnAntonyDv <anton.smit@hotmail.com>
2020-06-12 18:15:05 +0200

    back-up copy before pull

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const authRouter = require("./routers/auth");
 const orderRouter = require("./routers/order");
+const cors = require("cors");
+
 const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/auth", authRouter);
 app.use("/orders", orderRouter);
 
---
+++ routers/product.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+const Product = require("../models").product;
+const router = new Router();
+
+router.get("/", async (req, res, next) => {
+  const result = await Product.findAll();
+  console.log(result);
+  try {
+    if (!result) {
+      res.status(400).send("products not found");
+    } else {
+      res.json(result);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
