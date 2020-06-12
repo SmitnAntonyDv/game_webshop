3c78ddc5faf8a3e3f72ebebbda4beeb8acc10350
Jenni <jenni.svensson@live.se>
2020-06-12 13:46:40 +0200

    added categorie route

--- routers/categorie.js
+++ routers/categorie.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
-const Product = require("../models").product;
+const Category = require("../models").category;
 const router = new Router();
 
 router.get("/", async (req, res, next) => {
-  const result = await Product.findAll();
+  const result = await Category.findAll();
+  console.log(result);
   try {
     if (!result) {
-      res.status(400).send("products not found");
+      res.status(400).send("Category not found");
     } else {
       res.json(result);
     }
